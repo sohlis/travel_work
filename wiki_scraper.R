4bcdaf69bfc0be83ad1e9b2f3bb59b967e8106ce
Marley Buchman <marleyb@remi.com>
2017-02-20 22:41:44 -0800

    compiled all the scripts together

--- wiki_scraper.R
+++ wiki_scraper.R
@@ -2,6 +2,7 @@
 library(htmltab)
 library(dplyr)
 library(data.table)
+library(tidyverse)
 
 url <- "https://en.wikipedia.org/wiki/List_of_Diners,_Drive-Ins,_and_Dives_episodes"
 
@@ -94,8 +95,8 @@ data.for.url.scraper <- as.data.frame(data.for.url.scraper)
 flavor.town <- cbind(flavor.town, data.for.url.scraper)
 
 #create an index variable for the data frame
-index <- c(1:880)
-flavor.town <- cbind(flavor.town, index)
+#index <- c(1:880)
+#flavor.town <- cbind(flavor.town, index)
 
 #clean up the column names
 colnames(flavor.town)[colnames(flavor.town) == "Episode"] <- "ep_number"
@@ -144,12 +145,91 @@ flavor.town$restaurant <- gsub("â€˜", "'", flavor.town$restaurant)
 flavor.town <- flavor.town[!duplicated(flavor.town$google_string), ]
 
 #create an index variable for the data frame
-index <- c(1:880)
+index <- c(1:879)
 flavor.town <- cbind(flavor.town, index)
 
 #write a csv file for all the data frame
 #write.csv(flavor.town, file = "flavor_town_final.csv", row.names = FALSE)
 
+#####Connect the data from the wiki scraper to the yelp urls. This is an intermediate step to eventually merging all the data together
+#####Create final data frame that houses all data from DDD
+
+
+#read in csv file with restaurant names and urls
+names.url <- read_csv("names_with_url.csv")
+
+colnames(names.url)[colnames(names.url) == "Scully's Tavern Miami Florida yelp"] <- "google_string"
+colnames(names.url)[colnames(names.url) == "scullys-tavern-miami"] <- "yelp_string"
+
+#needing cleaning 
+names.url[585, 2] <- "worth-our-weight-santa-rosa"
+names.url[691, 2] <- "bun-n-barrel-san-antonio-2"
+names.url[749, 2] <- "falconettis-east-side-grill-vancouver"
+names.url[813, 2] <- "gold-n-silver-inn-reno"
+names.url[831, 2] <- "the-thumb-scottsdale"
+names.url[198, 2] <- NA 
+
+#create new merged data set
+ftown <- left_join(flavor.town, names.url, by = "google_string")
+
+#write new data set to file, begin preparing final merge 
+#write.csv(ftown, file = "all_data_sans_reviews.csv", row.names = FALSE)
+
+#read in review data
+reviews <- read_csv("merged.csv")
+
+#get rid of empty columns from import
+reviews <- reviews[, 1:4]
+
+#add in another column of all the reviews so that after I merge the data frames, I still have a variable to split the data frame by
+reviews$yelp_string <- reviews$`scullys-tavern-miami`
+
+#rename variables
+colnames(reviews)[colnames(reviews) == "scullys-tavern-miami"] <- "Restaurant"
+colnames(reviews)[colnames(reviews) == "10/14/16"] <- "review_date"
+colnames(reviews)[colnames(reviews) == "5.0 star rating"] <- "rating"
+colnames(reviews)[colnames(reviews) == "Finally made it \\\\xa0in to Scully's last night. \\\\xa0They play live Jazz on Thursday nights. \\\\xa0The Band: \\\\xa0Great 6 or 7 member ensemble playing jazzy favorites all night long. \\\\xa0We caught 2 sets and managed to dance a bit before we had to go home. \\\\xa0Definitely worth the trip, even if for the music alone.The Food: \\\\xa0Ordered wings and smoked fish dip. \\\\xa0Both were very good. \\\\xa0Menu has a great sampling of items. \\\\xa0Our friends had a seared tuna salad and a burger that also looked very good.The Drinks: \\\\xa0I drank Glenlevit neat all night. \\\\xa0You can't do anything to screw that up. \\\\xa0My wife had wine, OK selection considering where you are.The Service: \\\\xa0Excellent all the way around. \\\\xa0Our waitress, forgot to get her name, was great!!!! \\\\xa0She was very accommodating and looked in on some frequently. \\\\xa0 Bar and kitchen staff wee quick with orders also.Overall, I really liked this place alot. \\\\xa0I'm just sorry it took me nearly 30 years to make it there."] <- "review_text"
+
+#clean rating strings
+reviews$rating <- gsub(" star rating", "", reviews$rating)
+reviews$rating <- sub("[[:space:]]+$", "", reviews$rating) 
+reviews$rating <- as.numeric(reviews$rating)
+
+#clean dates
+reviews$review_date <- as.Date(reviews$review_date, format = "%m/%d/%y")
+
+#Merge ftown data set with reviews data set
+fieri <- merge(reviews, ftown, by = "yelp_string")
+
+#create before and after group
+fieri$group <- ifelse(fieri$review_date <= fieri$ep_air_date, "BEFORE", "AFTER")
+
+#write csv file containing final fieri data set
+#write.csv(fieri, "fieri_data_complete.csv", row.names = FALSE)
+
+#group by reviews before air date and take mean
+before_test <- fieri %>% 
+  filter(group == "BEFORE") %>% 
+  group_by(index) %>%
+  summarise_each(funs(mean), rating)
+
+colnames(before_test)[colnames(before_test) == "rating"] <- "avg_rating_before"
+
+#group by reviews after air date and take mean
+after_test <- fieri %>% 
+  filter(group == "AFTER") %>% 
+  group_by(index) %>%
+  summarise_each(funs(mean), rating)  
+
+colnames(after_test)[colnames(after_test) == "rating"] <- "avg_rating_after"
+
+#join before and after sets together
+effect_test <- left_join(before_test, after_test, by = "index")
+
+#calculate change in means before and after episode air date
+effect_test <- effect_test %>% 
+  mutate(score_change = avg_rating_after - avg_rating_before) %>% 
+  mutate(pct_score_change = (score_change / avg_rating_before) * 100)
 
 
 
