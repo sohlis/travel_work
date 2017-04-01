4eee32c44d68d0eb7a91ed56ca820f99c9631a8a
buchmayne <michaelmbuchman@gmail.com>
2017-04-01 12:04:18 -0400

    initial sentiment scoring of one restaurant

--- fieri_text.R
+++ fieri_text.R
@@ -2,6 +2,7 @@
 #Load necessary packages
 library(tidyverse)
 library(tidytext)
+library(stringr)
 
 #Read in data set generated from fieri_effect.R
 fieri_text <- read_csv("fieri_mac_march.csv")
@@ -39,23 +40,35 @@ tidy_review %>%
         xlab(NULL) +
         coord_flip()
 
-#Repeat word count visualization after aggregating all reviews for one restaurant
-agg_text_single_rest <- select(text_single_rest, review_text)
-agg_text_single_rest <- paste(agg_text_single_rest[, 1], collapse = "|")
+#Repeat for all the reviews for this particular restaurant 
+tidy_reviews <- text_single_rest %>% 
+        select(restaurant, review_text, rating, review_date) %>% 
+        unnest_tokens(word, review_text) %>% 
+        filter(!word %in% stop_words$word, str_detect(word, "^[a-z']+$"))
+tidy_reviews
 
-tidy_agg_text_rest <- data_frame(text = agg_text_single_rest)
-tidy_agg_review <- tidy_agg_text_rest %>% 
-        unnest_tokens(word, text) %>% 
-        anti_join(stop_words)
-
-tidy_agg_review %>% 
+#Visualize
+tidy_reviews %>% 
         count(word, sort = TRUE) %>% 
-        filter(n > 170) %>% 
+        filter(n > 250) %>% 
         mutate(word = reorder(word, n)) %>% 
         ggplot(aes(word, n)) +
         geom_bar(stat = 'identity') +
         xlab(NULL) +
         coord_flip()
 
+#Sentiment Analysis
+AFINN <- sentiments %>% 
+        filter(lexicon == "AFINN") %>% 
+        select(word, afinn_score = score)
 
+tidy_reviews_sentiment <- tidy_reviews %>% 
+        inner_join(AFINN, by = "word") %>% 
+        group_by(review_date, rating) %>% 
+        summarize(sentiment = mean(afinn_score))
+tidy_reviews_sentiment
 
+#Visualize Sentiment Analysis
+ggplot(tidy_reviews_sentiment, aes(rating, sentiment, group = rating)) + 
+        geom_boxplot() +
+        ylab("Average Sentiment Score")
